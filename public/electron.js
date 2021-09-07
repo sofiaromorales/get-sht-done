7a80502d416c7cd2c53c5eb0fa72ca0079107744
Sofia Rodriguez <sofiaromorales@gmail.com>
2021-09-07 10:35:25 -0400

    App ready for build

--- public/electron.js
+++ public/electron.js
@@ -3,22 +3,30 @@ const { app, BrowserWindow, ipcMain, ipcRenderer } = require('electron')
 const isDev = require('electron-is-dev')
 const Store = require('electron-store');
 
+let mainWindow = {
+  show: () => {
+    console.log("show");
+  }
+};
+
 function createWindow() {
     //Create the Browser Window
     const window = new BrowserWindow({
         widt: 800,
         height: 800,
+        backgroundColor: '#312450',
         webPreferences: {
             nodeIntegration: true,
             enableRemoteModule: true,
             preload: __dirname + '/preload.js'
         }
     })
-    window.loadURL(
-        isDev
-        ? 'http://localhost:3000'
-        : `file://${path.join(__dirname, "../build/index.html")}`
-    )
+    // window.loadURL(
+    //     isDev
+    //     ? 'http://localhost:3000'
+    //     :  `file://${path.join(__dirname, "../build/index.html")}`
+    // )
+    window.loadURL(`file://${path.join(__dirname, "../build/index.html")}`)
     if (isDev) {
         window.webContents.openDevTools({mode: 'detach'})
     }
@@ -42,6 +50,7 @@ app.on('window-all-closed', () => {
 })
 
 app.on('activate', () => {
+    mainWindow.show()
     if (BrowserWindow.getAllWindows().length === 0) {
         createWindow();
     }
